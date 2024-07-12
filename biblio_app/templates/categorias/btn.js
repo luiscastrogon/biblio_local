2fba5728937d36ee59fd78e8c5fb8a13488edabf
BryanGarrido <bry.garrido@duocuc.cl>
2024-07-12 18:54:05 -0400

    listado

---
+++ biblio_app/templates/categorias/btn.js
@@ -0,0 +1,7 @@
+$('#btn_categorias').click(function(){
+    Swal.fire({
+        title: "The Internet?",
+        text: "That thing is still around?",
+        icon: "question"
+      })
+});
--- biblio_app/views.py
+++ biblio_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import navbaritem, libro, autor, categoria
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -11,7 +12,15 @@ def index(request):
 def index2(request):
     navbaritems = navbaritem.objects.all()
     libros = libro.objects.all()
-    context = {"navbaritems": navbaritems, "libros": libros}
+    query = request.GET.get('query', '')
+    print(f"Query received: {query}") 
+    if query:
+        libros = libro.objects.filter(titulolib__iexact=query)
+    else:
+        libros = libro.objects.all()
+
+    context = {"navbaritems": navbaritems, "libros": libros ,
+        'query': query,}
     return render(request, 'catalogo_libro/index2.html', context)
 
 def index3(request):
@@ -25,3 +34,16 @@ def index4(request):
     categorias = categoria.objects.all()
     context = {"navbaritems": navbaritems, "categorias": categorias}
     return render(request, 'categorias/index4.html', context)
+
+def index5(request):
+    query = request.GET.get('query', '')
+    print(f"Query received: {query}") 
+    if query:
+        libros = libro.objects.filter(titulolib__iexact=query)
+    else:
+        libros = libro.objects.all()
+    context = {
+        'libros': libros,
+        'query': query,
+    }
+    return render(request, 'catalogo_libro/index2.html', context)
